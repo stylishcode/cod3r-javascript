96ff8fd23856f8ec78456b2083726143f4904434
Matheus Henrique <matheus.hpp135@gmail.com>
2022-05-03 01:46:44 -0300

    57 - simulando reduce

--- 57simulandoReduce.js
+++ 57simulandoReduce.js
@@ -1,11 +1,15 @@
-Array.prototype.meuReduce = function(callback) {
-  let acumulador = this[0];
-  for (i = 1; i < this.length; i++) {
+Array.prototype.meuReduce = function(callback, valorInicial) {
+  // Se for passado o valor o inicial, o indice inicial do for será 0 se não 1
+  const indiceInicial = valorInicial ? 0 : 1;
+  // Acumulador recebe this[0] ou se tiver sido passado, o indice inicial
+  let acumulador = valorInicial || this[0];
+  for (let i = indiceInicial; i < this.length; i++) {
+    // callback(acumulador, nums[i], i, nums)
     acumulador = callback(acumulador, this[i], i, this);
   }
   return acumulador;
 }
 
 const soma = (total, valor) => total + valor;
-const nums = [1, 2, 3, 4, 5];
-console.log(nums);
+const nums = [1, 2, 3, 4, 5, 6];
+console.log(nums.meuReduce(soma, 21));
